4b694068d5b9b86763fe967e94c673c19cb54e41
KrishnaKant Nagvanshi <88052084+krishnakant2001@users.noreply.github.com>
2023-12-26 23:21:40 +0530

    Add files via upload

---
+++ switch-case.cpp
@@ -0,0 +1,20 @@
+//switch-case
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+
+    switch(num){
+        case 1: cout << "KrishnaKant" << endl;
+                break;
+
+        case 2: cout << "Pratham" << endl;
+                break;
+
+        default : cout << "No name" << endl;
+    }
+    
+    return 0;
+}
